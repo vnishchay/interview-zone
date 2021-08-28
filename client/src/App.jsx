adc04a053219373e25509b110140534f03c93783
codernishchay <nishchay.vns2000@gmaiil.com>
2021-08-28 04:37:24 -0400

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,68 +1,27 @@
 import React, { useState } from "react";
-import Screen from "./components/Screen";
-import ChatArea from "./components/ChatArea";
-import Whiteboard from "./components/Whiteboard"; 
-import Video1 from "./components/Video1";
-import Video2 from "./components/Video2";
-import TextArea from "./components/TextArea";
-import styled, { css } from 'styled-components';
-
-
+import { BrowserRouter } from "react-router-dom";
+import { Route, } from "react-router";
+import { Switch } from "react-router";
+import CreateRoom from "./components/createRoom"; 
+import Room from "./components/room"; 
+import { black } from "color-name";
 function App() {
+
     
     return <div className="App" >
-<div  style={{
-  padding: 5,
-  display:"flex", 
-  flexDirection: "row"
-}}>
-    
-      <Whiteboard
-        style={{
 
-          position: "relative",
-       
-          height: screen.height, 
-          width : screen.width/2,
-        }}
-      ></Whiteboard>
-      <ChatArea
-        style={{
-          position: "relative",
-       
-          height: screen.height, 
-          width: screen.width/2,
-        }}
-      ></ChatArea>
-
-   </div> 
-		{/* <BrowserRouter>
-			<Switch>
+      
+      <BrowserRouter>
+        <Switch>
 				<Route path="/" exact component={CreateRoom}></Route>
+      
 				<Route path="/room/:roomID" component={Room}></Route>
+       
 			</Switch>
-		</BrowserRouter> */}
-    </div>
-
+      </BrowserRouter>
+         </div>
 }
 
 export default App;
-
-
-const Stack = styled.div`
-  width: 1367px;
-  height: 768px;
-  position: relative;
-  display: flex;
-`;
-
-const Rect = styled.div`
-  top: 85px;
-  left: 892px;
-  width: 100px;
-  background: black;
-  height: 100px;
-  position: absolute;
-`;
 
 
--- interviewService/app/handlers/roomHandler.go
+++ interviewService/app/handlers/roomHandler.go
@@ -71,3 +71,131 @@ func (r *RoomMap) DeleteRoom(roomID string) {
 
 	delete(r.Map, roomID)
 }
+
+///int solution()
+// {
+//     int n, mxVowel, mxConso, cntVowel, cntConso;
+//     string s;
+//     cin >> s;
+
+//     n = s.size();
+
+//     vector<int> letter(26, 0);
+
+//     for (int i = 0; i < n; i += 1)
+//     {
+//         letter[s[i] - 'A'] += 1;
+//     }
+
+//     mxVowel = mxConso = cntVowel = cntConso = 0;
+//     for (int i = 0; i < 26; i += 1)
+//     {
+//         if (letter[i])
+//         {
+//             if (i == 0 or i == 4 or i == 8 or i == 14 or i == 20)
+//             {
+//                 mxVowel = max(mxVowel, letter[i]);
+//                 cntVowel += letter[i];
+//             }
+//             else
+//             {
+//                 mxConso = max(mxConso, letter[i]);
+//                 cntConso += letter[i];
+//             }
+//         }
+//     }
+
+//     int res = (n - mxConso + (cntConso - mxConso));
+//     res = min(res, (n - mxVowel + (cntVowel - mxVowel)));
+//     return res;
+// }
+
+/**
+ *    author:  tourist1256
+ *    created: 27.08.2021 22:34:40
+**/
+/**
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 2351
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    cin >> tt;
+    for (int x = 1; x <= tt; x++) {
+        cout << "Case #" << x << ": ";
+        string str;
+        cin >> str;
+        if (count(str.begin(), str.end(), str[0]) == (int)str.length()) {
+            cout << "0\n";
+            continue;
+        }
+        set<char> vowel = {'A', 'E', 'I', 'O', 'U'};
+        map<char, int> v, c;
+        int cntV = 0, cntC = 0;
+        for (auto &ch : str) {
+            if (vowel.find(ch) != vowel.end()) {
+                cntV += 1;
+                v[ch] += 1;
+            } else {
+                cntC += 1;
+                c[ch] += 1;
+            }
+        }
+        if (v.empty()) {
+            cout << (int)str.length() << "\n";
+            continue;
+        }
+        int maxi = 0;
+        char termV, termC;
+        for (auto &it : c) {
+            if (maxi < it.second) {
+                termC = it.first;
+                maxi = it.second;
+            }
+        }
+        maxi = 0;
+        for (auto &it : v) {
+            if (maxi < it.second) {
+                termV = it.first;
+                maxi = it.second;
+            }
+        }
+        // cerr << termC << " " << termV << "\n";
+        auto change = [&](char toChange) {
+            int steps = 0;
+            for (auto &it : str) {
+                if (it == toChange) {
+                    continue;
+                }
+                if (vowel.find(it) != vowel.end()) {
+                    if (vowel.find(toChange) != vowel.end()) {
+                        steps += 2;
+                    } else {
+                        steps += 1;
+                    }
+                } else {
+                    if (vowel.find(toChange) != vowel.end()) {
+                        steps += 1;
+                    } else {
+                        steps += 2;
+                    }
+                }
+            }
+            return steps;
+        };
+        int ans1 = change(termC);
+        int ans2 = change(termV);
+        cout << min(ans1, ans2) << "\n";
+    }
+    return 0;
+}
+**/
